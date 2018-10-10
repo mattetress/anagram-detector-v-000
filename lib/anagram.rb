320c60d073b4d7f1433b69bee6a0d3b61585217c
Matt Etress <mattetress@me.com>
2018-10-10 19:47:05 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,15 @@
 class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
 
+  def match(list)
+    matches = []
+    list.each do |test_word|
+      matches << test_word if test_word.split("").sort == @word.split("").sort
+    end
+    matches
+  end
 end
